916cd926cdd2a28062f435a7a169da5284aabe17
백진암 <jinam1234567@gmail.com>
2024-04-22 10:22:45 +0900

    FIX: Gravity Zone sclae 조작 변경, -scale 버그해결

--- Assets/GravityMaker.cs
+++ Assets/GravityMaker.cs
@@ -10,6 +10,7 @@ public class GravityMaker : MonoBehaviour
     private Vector3 initialGravityFieldPosition;
     private Vector3 initialGravityFieldNormal;
     private Vector3 gravityZoneVector;
+    private Transform childGravityZone;
     private Transform gravityZone;
 
     [SerializeField] private GameObject gravityZonePrefab;
@@ -27,24 +28,36 @@ public class GravityMaker : MonoBehaviour
             {
                 (initialGravityFieldPosition, initialGravityFieldNormal) = Mouse3D.GetMousePositionAndNormal();
                 gravityZone = Instantiate(gravityZonePrefab, initialGravityFieldPosition, Quaternion.LookRotation(initialGravityFieldNormal)).transform;
+                childGravityZone = gravityZone.GetChild(0);
 
                 gravityZoneVector = gravityZone.position;
                 gravityZone.localScale = new Vector3(1, 1, 0.0001f);
             }
 
             Vector3 curPoint = Mouse3D.GetMousePosition();
-
             Vector3 distanceVector = curPoint - gravityZoneVector;
-            float adjustedDistance = Vector3.Dot(distanceVector, gravityZone.up);
 
-            float scaleSize = adjustedDistance * 0.5f * Mathf.Sqrt(2);
-            Vector3 gravityZoneLocalScale = Vector3.one * scaleSize;
-            gravityZoneLocalScale.z = 0.01f;
+            float scaleX = Vector3.Dot(distanceVector, gravityZone.right);
+            float scaleY = Vector3.Dot(distanceVector, gravityZone.up);
+            float scaleZ = 0.01f;
+
+            Vector3 gravityZoneLocalScale = new Vector3(scaleX, scaleY, scaleZ);
             gravityZone.localScale = gravityZoneLocalScale;
+
+            childGravityZone.localScale = ConvertVectorToOne(gravityZoneLocalScale);
         }
         else
         {
             initialGravityFieldPosition = Vector3.zero;
         }
+    }
+
+    Vector3 ConvertVectorToOne(Vector3 inputVector)
+    {
+        return new Vector3(
+            Mathf.Sign(inputVector.x),
+            Mathf.Sign(inputVector.y),
+            Mathf.Sign(inputVector.z)
+        );
     }
 }
