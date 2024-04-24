a08018947ed469395dc4e0b4e15b58b081ef1e9a
백진암 <jinam1234567@gmail.com>
2024-04-24 11:22:50 +0900

    ADD: Gravity

--- Assets/PlayerLocomotion.cs
+++ Assets/PlayerLocomotion.cs
@@ -8,16 +8,24 @@ public class PlayerLocomotion : MonoBehaviour
     private InputManager inputManager;
     private Rigidbody rb;
 
+    [Header("MOVEMENT")]
     [SerializeField] private Vector3 moveDirection;
-
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float mouseSpeed;
 
+    [Header("CAMERA")]
+    [SerializeField] private float mouseSpeed;
     [SerializeField] private float cameraVerticalAngle;
     [SerializeField] private float cameraHorizontalAngle;
-
     [SerializeField] private float maxAngle;
     [SerializeField] private Transform cameraPivot;
+
+    [Header("GRAVITY")]
+    public bool isGrounded;
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Transform groundChecker;
+    [SerializeField] private float gravityForce;
+    [SerializeField] private float gravityMultiplier;
+    [SerializeField] private LayerMask groundLayerMask;
     private void Awake()
     {
         inputManager = GetComponent<InputManager>();
@@ -26,6 +34,7 @@ public class PlayerLocomotion : MonoBehaviour
 
     public void HandleAllMovement()
     {
+        ApplyGravity();
         HandleMovement();
         HandleRotationCamera();
     }
@@ -34,8 +43,7 @@ public class PlayerLocomotion : MonoBehaviour
         moveDirection = cameraPivot.forward * inputManager.verticalInput;
         moveDirection += cameraPivot.right * inputManager.horizontalInput;
         moveDirection.Normalize();
-        moveDirection.x = 0;
-        moveDirection *= moveSpeed  ;
+        moveDirection *= moveSpeed;
         rb.velocity = moveDirection;
     }
     private void HandleRotationCamera()
@@ -53,5 +61,21 @@ public class PlayerLocomotion : MonoBehaviour
         rotation.x = cameraVerticalAngle;
         targetRotation = Quaternion.Euler(rotation);
         Camera.main.transform.localRotation = targetRotation;
+    }
+
+    float inAirTimer;
+    private void ApplyGravity()
+    {
+        isGrounded = Physics.SphereCast(groundChecker.position, 0.2f, -groundChecker.up, out RaycastHit hit, 0.2f, groundLayerMask);
+
+        if (!isGrounded)
+        {
+            inAirTimer += Time.deltaTime;
+            rb.AddForce(-playerTransform.up * gravityForce * gravityMultiplier * inAirTimer);
+        }
+        else
+        {
+            inAirTimer = 0;
+        }
     }
 }
