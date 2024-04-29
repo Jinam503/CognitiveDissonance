11dcc2eeca1bac826d54e562e3414f847124c131
백진암 <jinam1234567@gmail.com>
2024-04-29 15:24:33 +0900

    한글 폰트추가

---
+++ Assets/Scripts/ActionButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionButton : IInteractable
+{
+    
+    public void Interact()
+    {
+        
+    }
+}
